47beabf316527452b448917077a5ce776516ea2f
nsaviour <wenjian.22@163.com>
2025-08-28 16:22:08 +0800

    添加xlsx依赖，添加测试页面路径

--- chat-agent-app/src/router/index.js
+++ chat-agent-app/src/router/index.js
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/business-logic-test',
+      name: 'business-logic-test',
+      component: () => import('../components/business-logic-test.vue'),
+    },
   ],
 })
 
